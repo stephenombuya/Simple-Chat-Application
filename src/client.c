b7fbbd731cf977e1bc1e8213e0058e5381ac48c6
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-02-13 19:17:45 +0300

    Update client.c

--- src/client.c
+++ src/client.c
@@ -9,9 +9,16 @@
 #define BUFFER_SIZE 1024
 #define SERVER_IP "127.0.0.1"
 
-void error(char *msg) {
+void error(const char *msg) {
     perror(msg);
-    exit(1);
+    exit(EXIT_FAILURE);
+}
+
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
 }
 
 int main() {
@@ -19,50 +26,46 @@ int main() {
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
-    // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         error("Error opening socket");
     }
 
-    // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    
+
     if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
         error("Invalid address");
     }
 
-    // Connect to server
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         error("Connection failed");
     }
 
-    printf("Connected to server\n");
+    printf("Connected to server. Type 'exit' to disconnect.\n");
 
-    // Chat loop
     while (1) {
-        // Clear buffer
-        memset(buffer, 0, BUFFER_SIZE);
-
-        // Get user input
         printf("Client: ");
         fgets(buffer, BUFFER_SIZE, stdin);
+        trim_newline(buffer);
 
-        // Send message
+        if (strcmp(buffer, "exit") == 0) {
+            printf("Disconnecting...\n");
+            break;
+        }
+
         if (send(sock, buffer, strlen(buffer), 0) < 0) {
             error("Error sending message");
         }
 
-        // Clear buffer and receive response
         memset(buffer, 0, BUFFER_SIZE);
         if (recv(sock, buffer, BUFFER_SIZE, 0) <= 0) {
             printf("Server disconnected\n");
             break;
         }
 
-        printf("Server: %s", buffer);
+        printf("Server: %s\n", buffer);
     }
 
     close(sock);
